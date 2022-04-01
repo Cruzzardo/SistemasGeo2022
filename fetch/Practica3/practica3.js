bfe959e27aa5c2cddc6dc312d778cb814bbdfc96
Cruzzard <can71695@udelasalle.edu.mx>
2022-03-31 18:13:03 -0600

    .

--- fetch/Practica3/practica3.js
+++ fetch/Practica3/practica3.js
@@ -34,60 +34,19 @@ function iniciaMapa(){
 
     console.log(marcadores);
 
+    var icono = {
+        url : "https://media.giphy.com/media/3oEjIacPJYAJubIDLO/giphy.gif",
+        scaledSize: new google.maps.Size(50,50),
+        origin: new google.maps.Point(0,0),
+        anchor: new google.maps.Point(0,0)
+    }
+
     for( marcador of  marcadores){
 
         let marker = new google.maps.Marker({
             map: map,
-            position: new google.maps.LatLng(marcador.latitude, marcador.longitude),
+            position: new google.maps.LatLng(icono),
             title: marcador.name
         });
     }
 };
-
-const centerCoordinates = ol.proj.fromLonLat([-3.74922, 40.463667,]);
-const initialZoom = 5
-
-
-const view = new ol.View({
-  center: centerCoordinates,
-  zoom: initialZoom
-})
-
-const osmLayer = new ol.layer.Tile({
-  source: new ol.source.OSM()
-})
-
-const map = new ol.Map({
-  target: 'map',
-  layers: [osmLayer],
-  view: view
-});
-
-
-function zoomTo(coordinates, zoom) {
-  map.getView().setCenter(coordinates)
-  map.getView().setZoom(zoom);
-}
-
-let initViewButton = document.getElementById('init-view');
-initViewButton.addEventListener('click', () => zoomTo(centerCoordinates, initialZoom));
-
-function getZoomLevel() {
-  let currentZoom = Math.round(map.getView().getZoom());
-
-  alert(`Current Zoom Level: ${currentZoom}`)
-}
-
-let viewZoomButton = document.getElementById('view-zoom');
-viewZoomButton.addEventListener('click', getZoomLevel);
-
-function getCurrentExtent() {
-  let currentExtent = map.getView().calculateExtent(map.getSize());
-  let projectionCode = map.getView().getProjection().code_;
-  let transformExtent = ol.proj.transformExtent(currentExtent, projectionCode, 'EPSG:4326');
-
-  alert(`Extend: ${transformExtent.toString()}`)
-}
-
-let viewExtentButton = document.getElementById('view-extent');
-viewExtentButton.addEventListener('click', getCurrentExtent);
